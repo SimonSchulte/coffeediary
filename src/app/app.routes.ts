0ada7907412eca907467446500bf6a3fb8e4cf2a
Simon Schulte <s.schulte@diamant-software.de>
2025-11-15 20:35:14 +0100

    readded footer and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/espressos', pathMatch: 'full' },
-  { path: 'espressos', loadComponent: () => import('./espressos/espressos.component').then(m => m.EspressosComponent) }
+  { path: 'espressos', loadComponent: () => import('./espressos/espressos.component').then(m => m.EspressosComponent) },
+  { path: 'purover', loadComponent: () => import('./espressos/espressos.component').then(m => m.EspressosComponent) }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,24 +3,28 @@ import {RouterOutlet} from '@angular/router';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {SupabaseAuthService} from './services/supabase-auth.service';
 import {MatButton} from '@angular/material/button';
+import {AppToolbar} from './ui/app-toolbar.component';
+import {AppFooter} from './ui/app-footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSnackBarModule, MatButton],
+  imports: [RouterOutlet, MatSnackBarModule, MatButton, AppToolbar, AppFooter],
   template: `
+    <app-toolbar></app-toolbar>
     @if (!loggedIn) {
       <div class="auth-block">
         <h2>Login erforderlich</h2>
         <button mat-flat-button color="primary" (click)="login()">Mit Google anmelden</button>
       </div>
     } @else {
-<!--      <div class="user-block">-->
-<!--        <span>Angemeldet als: {{ auth.getUser()?.email }}</span>-->
-<!--        <button mat-flat-button color="primary" (click)="logout()">Logout</button>-->
-<!--      </div>-->
-      <!-- ...bestehende App-Inhalte... -->
+      <!--      <div class="user-block">-->
+        <!--        <span>Angemeldet als: {{ auth.getUser()?.email }}</span>-->
+        <!--        <button mat-flat-button color="primary" (click)="logout()">Logout</button>-->
+        <!--      </div>-->
+        <!-- ...bestehende App-Inhalte... -->
       <router-outlet></router-outlet>
     }
+    <app-footer></app-footer>
   `,
   styleUrl: './app.scss'
 })
